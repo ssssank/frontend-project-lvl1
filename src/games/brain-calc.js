2b496aef184f583d726620a6ae3fe68f0abe8c0a
Alex Litvinov <ssssank@gmail.com>
2020-01-11 16:25:20 +0300

    Delete mutation in brain-calc

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -4,39 +4,30 @@ import startGame from '..';
 
 const rule = 'What is the result of the expression?';
 
+const operators = ['+', '-', '*'];
+const maxNumberDefault = 50;
+const maxNumberForMultiply = 11;
+
 const getRightAnswer = (firstNumber, secondNumber, operator) => {
   switch (operator) {
     case ('+'):
       return +firstNumber + +secondNumber;
     case ('-'):
       return firstNumber - secondNumber;
-    default:
+    case ('*'):
       return firstNumber * secondNumber;
+    default:
+      return null;
   }
 };
 
-const generateRandomOper = () => {
-  switch (Math.round(Math.random() * 3)) {
-    case (0):
-      return '+';
-    case (1):
-      return '-';
-    default:
-      return '*';
-  }
-};
+const generateRandomOper = () => operators[generateRandomInt(0, operators.length)];
 
 const prepareRound = () => {
   const operator = generateRandomOper();
-  let firstNumber;
-  let secondNumber;
-  if (operator === '*') {
-    firstNumber = generateRandomInt(2, 11);
-    secondNumber = generateRandomInt(2, 11);
-  } else {
-    firstNumber = generateRandomInt();
-    secondNumber = generateRandomInt();
-  }
+  const maxNumberForGeneration = operator === '*' ? maxNumberForMultiply : maxNumberDefault;
+  const firstNumber = generateRandomInt(0, maxNumberForGeneration);
+  const secondNumber = generateRandomInt(0, maxNumberForGeneration);
   const question = `${firstNumber} ${operator} ${secondNumber}`;
   const rightAnswer = getRightAnswer(firstNumber, secondNumber, operator);
   return cons(question, rightAnswer.toString());
