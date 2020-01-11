2f71d79b400049350c1f76dc4c264d15099e8bfd
Alex Litvinov <ssssank@gmail.com>
2020-01-11 16:35:05 +0300

    Make generateProgression function pure

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -5,19 +5,19 @@ import startGame from '..';
 const rule = 'What number is missing in the progression?';
 const progressionLength = 10;
 
-const generateRandomProgression = (length) => {
-  const progressionStart = generateRandomInt();
-  const progressionStep = generateRandomInt();
+const generateProgression = (length, start, step) => {
   const progression = [];
   for (let i = 0; i < length; i += 1) {
-    progression.push(progressionStart + progressionStep * i);
+    progression.push(start + step * i);
   }
   return progression;
 };
 
 const prepareRound = () => {
-  const progression = generateRandomProgression(progressionLength);
-  const randomIndex = generateRandomInt(0, 10);
+  const progressionStart = generateRandomInt();
+  const progressionStep = generateRandomInt();
+  const progression = generateProgression(progressionLength, progressionStart, progressionStep);
+  const randomIndex = generateRandomInt(0, progressionLength);
   const rightAnswer = progression[randomIndex];
   progression[randomIndex] = '..';
   return cons(progression.join(' '), rightAnswer.toString());
